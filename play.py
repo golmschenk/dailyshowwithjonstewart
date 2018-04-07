f12bd27e120e31b0f64099b61b477707238d1114
Greg Olmschenk <golmschenk@gmail.com>
2018-04-07 11:41:17 -0400

    Add working video generator

--- play.py
+++ play.py
@@ -1,6 +1,16 @@
-
-from selenium import webdriver
+import os
+import time
+import shutil
+import itertools
+import subprocess
 import youtube_dl
+from selenium import webdriver
+
+episode_link = 'http://www.cc.com/video-clips/wbjagy/the-daily-show-with-jon-stewart-headlines---rockey-road-to-dc'
+
+if os.path.exists('temporary'):
+    shutil.rmtree('temporary')
+os.makedirs('temporary')
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
@@ -8,21 +18,37 @@ chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument("--mute-audio")
 browser = webdriver.Chrome(chrome_options=chrome_options)
 
-starting_link = 'http://www.cc.com/video-clips/wrsbno/the-daily-show-with-jon-stewart-presidents--day'
-
-with youtube_dl.YoutubeDL() as downloader:
+youtube_dl_options = {'outtmpl': 'temporary/%(autonumber)s.%(ext)s'}
+episode_date_string = None
+with youtube_dl.YoutubeDL(youtube_dl_options) as downloader:
     try:
-        next_link = starting_link
-        for _ in range(10):
-            downloader.download([next_link])
+        next_link = episode_link
+        for index in itertools.count():
             browser.get(next_link)
+            time.sleep(1)
             embed_button = browser.find_element_by_css_selector('.player_share-button.embed.button')
-            current_thumb_nail = browser.find_element_by_css_selector('.item.active.is-selected')
+            current_thumb_nail = browser.find_element_by_css_selector('.item.is-selected')
             next_thumb_nail = current_thumb_nail.find_element_by_xpath('following-sibling::div')
             anchor = next_thumb_nail.find_element_by_tag_name('a')
+            anchor_header = anchor.find_element_by_class_name('meta-wrap').find_element_by_class_name('header')
+            anchor_date_string = anchor_header.find_elements_by_tag_name('span')[1].text
+            if episode_date_string is None:
+                episode_date_string = anchor_date_string
+            if anchor_date_string != episode_date_string:
+                break
             next_link = anchor.get_attribute('href')
+            downloader.download([next_link])
         print(next_link)
         browser.quit()
     except Exception as error:
         browser.quit()
         raise error
+
+episode_date_string = episode_date_string.replace('/', '-')
+clip_list = sorted(os.listdir('temporary'))
+with open('temporary/concat.txt', 'a+') as concat_file:
+    for clip_name in clip_list:
+        concat_file.write('file {}\n'.format(clip_name))
+ffmpeg_call_list = ['ffmpeg', '-y', '-f', 'concat', '-i', 'temporary/concat.txt',
+                    'output/{}.mp4'.format(episode_date_string)]
+subprocess.call(ffmpeg_call_list)
